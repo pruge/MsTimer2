12ba1d41a3565b9d74cdab652949237510325453
james kim <strive1024@gmail.com>
2024-08-29 15:53:00 +0900

    support class service routine

--- MsTimer2.h
+++ MsTimer2.h
@@ -10,16 +10,20 @@
 #endif
 
 namespace MsTimer2 {
-	extern unsigned long msecs;
-	extern void (*func)();
-	extern volatile unsigned long count;
-	extern volatile char overflowing;
-	extern volatile unsigned int tcnt2;
-	
-	void set(unsigned long ms, void (*f)());
-	void start();
-	void stop();
-	void _overflow();
-}
+extern unsigned long msecs;
+extern void (*func)();
+extern void (*func2)(void *);
+extern void *_FuncParam;
+extern volatile unsigned long count;
+extern volatile char overflowing;
+extern volatile unsigned int tcnt2;
+
+void set(unsigned long ms);
+void set(unsigned long ms, void (*f)());
+void set(unsigned long ms, void (*f)(void *), void *parameter);
+void start();
+void stop();
+void _overflow();
+} // namespace MsTimer2
 
 #endif
